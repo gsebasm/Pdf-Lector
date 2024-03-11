a4f5f07d97fcdc4dec0704d62f58daf54691f721
Sebastian Garza <gsebasm8@gmail.com>
2024-03-10 22:41:45 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader= PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
 
 def main():
+    load_dotenv()
     st.set_page_config(page_title="Chat con varios PDFs", page_icon="books:")
 
     st.header("Chat con varios PDFs :books:")
@@ -8,8 +20,17 @@ def main():
 
     with st.sidebar:
         st.subheader("Tus documentos")
-        st.file_uploader("Sube tus pdfs aqui y click en 'Procesar'")
-        st.button("Process")
+        pdf_docs = st.file_uploader(
+            "Sube tus pdfs aqui y click en 'Procesar'", accept_multiple_files=True )
+        if st.button("Process"):
+           with st.spinner("Procesando"):
+            #se obtiene el pdf
+               raw_text = get_pdf_text(pdf_docs)
+
+            #los textos se separan chunks
+
+            #creando los vectores
+
     
     
     
