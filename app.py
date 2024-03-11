0161dd29149dbd973c33dc9b33a8553275e31f40
Sebastian Garza <gsebasm8@gmail.com>
2024-03-10 21:11:16 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,16 @@ import streamlit as st
 
 def main():
     st.set_page_config(page_title="Chat con varios PDFs", page_icon="books:")
+
+    st.header("Chat con varios PDFs :books:")
+    st.chat_input("Pregunta sobre tus documetos")
+
+    with st.sidebar:
+        st.subheader("Tus documentos")
+        st.file_uploader("Sube tus pdfs aqui y click en 'Procesar'")
+        st.button("Process")
+    
+    
     
 
 
