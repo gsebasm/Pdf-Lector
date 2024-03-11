14f3d9fb14e77c4abfd090583f43ec66b7c29cc8
Sebastian Garza <gsebasm8@gmail.com>
2024-03-11 00:05:50 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -10,7 +13,23 @@ def get_pdf_text(pdf_docs):
             text += page.extract_text()
     return text
 
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
 
+def get_vectorstore(text_chunks):
+    #embeddings = OpenAIEmbeddings()
+    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
+    
 def main():
     load_dotenv()
     st.set_page_config(page_title="Chat con varios PDFs", page_icon="books:")
@@ -26,10 +45,14 @@ def main():
            with st.spinner("Procesando"):
             #se obtiene el pdf
                raw_text = get_pdf_text(pdf_docs)
+               st.write(raw_text)
 
             #los textos se separan chunks
+               text_chunks = get_text_chunks(raw_text)
+               st.write(text_chunks)
 
             #creando los vectores
+               vectorstore= get_vectorstore(text_chunks)
 
     
     
