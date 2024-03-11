1d38ae6f8ec4c05edb89b0236a57309ae0de6ee5
Sebastian Garza <gsebasm8@gmail.com>
2024-03-11 01:18:32 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
 from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -24,18 +28,43 @@ def get_text_chunks(text):
     return chunks
 
 def get_vectorstore(text_chunks):
-    #embeddings = OpenAIEmbeddings()
-    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    embeddings = OpenAIEmbeddings()
+    #embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
-    
+def get_conservation_chain(vectorstore):
+    llm = ChatOpenAI()
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm = llm,
+        retriever = vectorstore.as_retriever(),
+        memory = memory
+    )
+    return conversation_chain
+
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.write(response)
+
 def main():
     load_dotenv()
     st.set_page_config(page_title="Chat con varios PDFs", page_icon="books:")
 
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+
     st.header("Chat con varios PDFs :books:")
-    st.chat_input("Pregunta sobre tus documetos")
+    user_question = st.chat_input("Pregunta sobre tus documetos")
+
+    if user_question:
+        handle_userinput(user_question)
+
+    with st.chat_message("user"):
+     st.write("Hello 👋")
+
+    with st.chat_message("assistant"):
+     st.write("Hello 👋")
 
     with st.sidebar:
         st.subheader("Tus documentos")
@@ -53,6 +82,10 @@ def main():
 
             #creando los vectores
                vectorstore= get_vectorstore(text_chunks)
+
+            #creando la chain y conversacion 
+               st.session_state.conservation = get_conservation_chain(vectorstore)
+   
 
     
     
