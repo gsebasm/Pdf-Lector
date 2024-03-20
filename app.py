d0ac514d575d5424e94ee4228e8fa018346e1ffa
Sebastian Garza <gsebasm8@gmail.com>
2024-03-20 01:36:31 -0600

    Espacios test

--- app.py
+++ app.py
@@ -93,7 +93,5 @@ def main():
             #creando la chain y conversacion 
                st.session_state.conversation = get_conversation_chain(vectorstore)
    
-
-
 if __name__ == '__main__':
     main()
